2ffd436d0feff7e210ea02fc41d7bbc6e80adb63
Kyle E. Mitchell <kyle@kemitchell.com>
2017-08-26 01:44:06 -0700

    Remove unused helper module

--- fail.js
+++
@@ -1,8 +0,0 @@
-module.exports = function (error) {
-  if (typeof error === 'string') {
-    console.error(error)
-  } else {
-    console.error(error.userMessage || error)
-  }
-  process.exit(1)
-}
