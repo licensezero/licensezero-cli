d63c096a97a8bf7efd4db0180c0822ad14ac11f3
Kyle E. Mitchell <kyle@kemitchell.com>
2017-08-26 02:25:51 -0700

    Refactor test helper

--- test/helper.js
+++ test/helper.js
@@ -7,26 +7,20 @@ var rimraf = require('rimraf')
 var streamBuffers = require('stream-buffers')
 
 module.exports = function (callback) {
-  fs.mkdtemp('/tmp/', ecb(fail, function (tmp) {
-    var config = path.join(tmp, 'config')
+  fs.mkdtemp('/tmp/', ecb(fail, function (directory) {
+    var config = path.join(directory, 'config')
     mkdirp(config, ecb(fail, function () {
       callback(
-        tmp,
+        directory,
         function run (cli, args, next) {
           var stdout = new streamBuffers.WritableStreamBuffer()
           var stderr = new streamBuffers.WritableStreamBuffer()
           cli(
-            args, tmp, config, stdout, stderr,
+            args, directory, config, stdout, stderr,
             function (error) {
-              var status
-              if (error) {
-                stderr.write(formatError(error))
-                status = 1
-              } else {
-                status = 0
-              }
+              if (error) stderr.write(formatError(error))
               next(
-                status,
+                error ? 1 : 0,
                 stdout.getContentsAsString('utf8') || '',
                 stderr.getContentsAsString('utf8') || ''
               )
@@ -34,7 +28,7 @@ module.exports = function (callback) {
           )
         },
         function rm (test) {
-          rimraf(tmp, ecb(fail, function () {
+          rimraf(directory, ecb(fail, function () {
             test.end()
           }))
         }
