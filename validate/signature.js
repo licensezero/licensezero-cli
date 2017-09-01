3959c6d624dd9d01e9558e09eecf9664373dd7b5
Kyle E. Mitchell <kyle@kemitchell.com>
2017-09-01 08:37:55 -0700

    Replace ed25519 with ed25519-supercop

--- validate/signature.js
+++ validate/signature.js
@@ -1,9 +1,9 @@
-var ed25519 = require('ed25519')
+var ed25519 = require('ed25519-supercop')
 
 module.exports = function (object) {
-  return ed25519.Verify(
-    Buffer.from(object.manifest + '\n\n' + object.document, 'utf8'),
-    Buffer.from(object.publicKey, 'hex'),
-    Buffer.from(object.signature, 'hex')
+  return ed25519.verify(
+    object.manifest + '\n\n' + object.document,
+    object.publicKey,
+    object.signature
   )
 }
