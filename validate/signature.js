611b205be95c886b79d2651fdd1b9793dc000623
Kyle E. Mitchell <kyle@kemitchell.com>
2017-09-11 22:00:37 -0700

    Replace ed25519-supercop with tweetnacl

--- validate/signature.js
+++ validate/signature.js
@@ -1,9 +1,9 @@
-var ed25519 = require('ed25519-supercop')
+var ed25519 = require('tweetnacl')
 
 module.exports = function (object) {
-  return ed25519.verify(
-    object.manifest + '\n\n' + object.document,
-    object.publicKey,
-    object.signature
+  return ed25519.sign.detached.verify(
+    Buffer.from(object.manifest + '\n\n' + object.document),
+    Buffer.from(object.signature, 'hex'),
+    Buffer.from(object.publicKey, 'hex')
   )
 }
