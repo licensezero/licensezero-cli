252aea12c71969cdc385ee47ceda066a515d85a6
Kyle E. Mitchell <kyle@kemitchell.com>
2017-09-11 22:15:30 -0700

    mkdirp before writing JSON files

--- write/json-file.js
+++ write/json-file.js
@@ -1,5 +1,11 @@
 var fs = require('fs')
+var mkdirp = require('mkdirp')
+var path = require('path')
+var runSeries = require('run-series')
 
 module.exports = function (file, data, callback) {
-  fs.writeFile(file, JSON.stringify(data), callback)
+  runSeries([
+    mkdirp.bind(null, path.dirname(file)),
+    fs.writeFile.bind(null, file, JSON.stringify(data))
+  ], callback)
 }
