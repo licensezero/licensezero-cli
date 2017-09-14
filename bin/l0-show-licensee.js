77e0885fc6eb713e94f8c5c55c1f2ce4cc3cb80f
Kyle E. Mitchell <kyle@kemitchell.com>
2017-09-13 19:11:28 -0700

    l0-show-licensee: list waivers

--- bin/l0-show-licensee.js
+++ bin/l0-show-licensee.js
@@ -15,13 +15,16 @@ module.exports = function (argv, cwd, config, stdin, stdout, stderr, done) {
 
   var nickname = options['<nickname>']
 
-  // TODO: list waivers
-
   var readLicensee = require('../read/licensee')
   readLicensee(config, nickname, function (error, licensee) {
     if (error) return done(error)
+    var runParallel = require('run-parallel')
     var readLicenses = require('../read/licenses')
-    readLicenses(config, nickname, function (error, licenses) {
+    var readWaivers = require('../read/waivers')
+    runParallel({
+      licenses: readLicenses.bind(null, config, nickname),
+      waivers: readWaivers.bind(null, config, nickname)
+    }, function (error, results) {
       if (error) return done(error)
       var lamos = require('lamos')
       stdout.write(
@@ -30,14 +33,23 @@ module.exports = function (argv, cwd, config, stdin, stdout, stderr, done) {
           'Legal Name': licensee.name,
           'Jurisdiction': licensee.jurisdiction,
           'License Tier': licensee.tier,
-          'Licenses': licenses.length === 0
+          'Licenses': results.licenses.length === 0
             ? 'None'
-            : licenses.map(function (license) {
-              return license.projectID
-            })
+            : results.licenses.map(formatDocument),
+          'Waivers': results.waivers.length === 0
+            ? 'None'
+            : results.waivers.map(formatDocument)
         }) + '\n'
       )
       return done()
+      function formatDocument (argument) {
+        var manifest = JSON.parse(argument.manifest)
+        return {
+          'Project': argument.projectID,
+          'Description': manifest.project.description,
+          'Repository': manifest.project.repository
+        }
+      }
     })
   })
 }
