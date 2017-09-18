90a297690508387d3f470c5c290eb831b21fc559
Kyle E. Mitchell <kyle@kemitchell.com>
2017-09-18 07:34:20 -0700

    l0-license: Support noncommercial and reciprocal

--- bin/l0-license.js
+++ bin/l0-license.js
@@ -3,18 +3,21 @@ module.exports = function (argv, cwd, config, stdin, stdout, stderr, done) {
     'Apply the License Zero Public License to an npm package.',
     '',
     'Usage:',
-    '  l0-license <UUID> [--quiet]',
+    '  l0-license <UUID> (--noncommercial | --reciprocal) [--quiet]',
     '  l0-license -h | --help',
     '  l0-license -v | --version',
     '',
     'Options:',
-    '  -h, --help     Print this screen to standard output.',
-    '  -v, --version  Print version to standard output.',
-    '  -q, --quiet    Suppress output.'
+    '  -h, --help           Print this screen to standard output.',
+    '  -v, --version        Print version to standard output.',
+    '  -q, --quiet          Suppress output.',
+    '  -n, --noncommercial  Apply the Noncommercial Public License.',
+    '  -r, --reciprocal     Apply the Reciprocal Public License.'
   ]).apply(null, arguments)
   if (!options) return
 
   var projectID = options['<UUID>']
+  var noncommercial = options['--noncommercial']
 
   var request = require('../request')
   request({
@@ -31,7 +34,8 @@ module.exports = function (argv, cwd, config, stdin, stdout, stderr, done) {
         action: 'public',
         licensorID: licensor.licensorID,
         token: licensor.token,
-        projectID: projectID
+        projectID: projectID,
+        terms: noncommercial ? 'noncommercial' : 'reciprocal'
       }, function (error, response) {
         /* istanbul ignore if */
         if (error) return done(error)
