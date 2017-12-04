9f5583901c28aae0836a9d7e3a5a9a9329093258
Kyle E. Mitchell <kyle@kemitchell.com>
2017-12-03 17:44:27 -0800

    Remove old TODOs
    `import-license` and `import-waiver` validate signatures separately.

--- validate/license.js
+++ validate/license.js
@@ -4,9 +4,6 @@ var licenseSchema = require('../schemas/license')
 var ajv = new AJV()
 var validate = ajv.compile(licenseSchema)
 
-// TODO validate agent signature
-// TODO validate licensor signature
-
 module.exports = function (argument) {
   return validate(argument)
 }
--- validate/waiver.js
+++ validate/waiver.js
@@ -4,9 +4,6 @@ var waiverSchema = require('../schemas/waiver')
 var ajv = new AJV()
 var validate = ajv.compile(waiverSchema)
 
-// TODO validate agent signature
-// TODO validate licensor signature
-
 module.exports = function (argument) {
   return validate(argument)
 }
