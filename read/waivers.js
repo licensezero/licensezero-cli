f1bba45644c3bbd5091bf6de3ab0303d6d217b92
Kyle E. Mitchell <kyle@kemitchell.com>
2017-12-03 16:48:30 -0800

    Ignore expired waivers (close #4)

--- read/waivers.js
+++ read/waivers.js
@@ -9,6 +9,29 @@ module.exports = function (config, nickname, callback) {
   fs.readdir(directory, eea(callback, function (entries) {
     runParallel(entries.map(function (projectID) {
       return readWaiver.bind(null, config, nickname, projectID)
-    }), callback)
+    }), function (error, waivers) {
+      if (error) return callback(error)
+      callback(null, waivers.filter(function (waiver) {
+        return !expired(waiver)
+      }))
+    })
   }))
+}
+
+var ONE_DAY = 24 * 60 * 60 * 1000
+var TODAY = new Date()
+
+function expired (waiver) {
+  var manifest
+  try {
+    manifest = JSON.parse(waiver.manifest)
+  } catch (error) {
+    return true
+  }
+  if (!manifest.hasOwnProperty('term')) return true
+  if (manifest.term === 'forever') return false
+  var term = parseInt(manifest.term)
+  var date = new Date(manifest.date)
+  if (isNaN(date)) return true
+  return (TODAY - date) < (term * ONE_DAY)
 }
