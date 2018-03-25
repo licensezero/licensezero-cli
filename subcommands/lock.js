1240df2b1a0454475643925372f58a00b308ac88
Kyle E. Mitchell <kyle@kemitchell.com>
2018-03-24 18:45:36 -0700

    Fix project ID printouts in success messages

--- subcommands/lock.js
+++ subcommands/lock.js
@@ -18,7 +18,7 @@ module.exports = function (options, cwd, config, stdin, stdout, stderr, done) {
     }, function (error, response) {
       if (error) return done(error)
       stdout.write(
-        'Project ID ' + response.projectID +
+        'Project ID ' + projectID +
         ' locked until ' + unlockDateString + '.'
       )
       done()
