79ab7e87b4556b213d71ad3422dc1a634795ab5f
Kyle E. Mitchell <kyle@kemitchell.com>
2018-03-25 14:48:27 -0700

    Remove redundant Y/N prompts

--- agree-to-terms.js
+++ agree-to-terms.js
@@ -7,11 +7,11 @@ module.exports = function (stdin, stdout, agency, callback) {
     agency
       ? (
         'Do you agree to the agency terms at ' +
-        'https://licensezero.com/terms/agency? [Y/N]'
+        'https://licensezero.com/terms/agency?'
       )
       : (
         'Do you agree to the terms of service at ' +
-        'https://licensezero.com/terms/service? [Y/N]'
+        'https://licensezero.com/terms/service?'
       ),
     callback
   )
