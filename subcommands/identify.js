6aa48b42c6603cf3bc62334dec012a26ac3ece86
Kyle E. Mitchell <kyle@kemitchell.com>
2018-03-26 12:10:11 -0700

    identify: Fix logic errors affecting prompts and validation

--- subcommands/identify.js
+++ subcommands/identify.js
@@ -1,40 +1,45 @@
 module.exports = function (options, cwd, config, stdin, stdout, stderr, done) {
+  var validations = {
+    name: require('../validate/name'),
+    jurisdiction: require('../validate/jurisdiction'),
+    email: require('../validate/email')
+  }
+  var keys = Object.keys(validations)
+  var newIdentity = {}
+  for (var index = 0; index < keys.length; index++) {
+    var key = keys[index]
+    var value = newIdentity[key] = options['<' + key + '>']
+    if (!validations[key](value)) return done('invalid ' + key)
+  }
   var readIdentities = require('../read/identities')
   readIdentities(config, function (error, identities) {
     /* istanbul ignore if */
     if (error) return done(error)
-
+    var identical = identities.find(function (existingIdentity) {
+      return (
+        existingIdentity.name === newIdentity.name &&
+        existingIdentity.jurisdiction === newIdentity.jurisdiction &&
+        existingIdentity.email === newIdentity.email
+      )
+    })
+    if (identical) return done('identical to existing identity')
     var confirm = require('../confirm')
     var prompt = [
       'You have already created an identity.',
       'Create another one?'
     ].join('\n')
-    confirm(stdin, stdout, prompt, function (error, accepted) {
-      if (error) return done(error)
-      if (!accepted) return done()
-      var validations = {
-        name: require('../validate/name'),
-        jurisdiction: require('../validate/jurisdiction'),
-        email: require('../validate/email')
-      }
-      var keys = Object.keys(validations)
-      var newIdentity = {}
-      for (var index = 0; index < keys.length; index++) {
-        var key = keys[index]
-        var value = newIdentity[key] = options['<' + key + '>']
-        if (!validations[key](value)) return done('invalid ' + key)
-      }
-
-      var identical = identities.find(function (existingIdentity) {
-        return (
-          existingIdentity.name === newIdentity.name &&
-          existingIdentity.jurisdiction === newIdentity.jurisdiction &&
-          existingIdentity.email === newIdentity.email
-        )
+    if (identities.length === 0) {
+      continueCreatingIdentity()
+    } else {
+      confirm(stdin, stdout, prompt, function (error, accepted) {
+        if (error) return done(error)
+        if (!accepted) return done()
+        continueCreatingIdentity()
       })
-      if (identical) return done('identical to existing identity')
+    }
+    function continueCreatingIdentity () {
       var writeIdentity = require('../write/identity')
       writeIdentity(config, newIdentity, done)
-    })
+    }
   })
 }
